2c97a6357b4b9355a3474d3c12cd86f0412c7a79
Julien Duroure <julien.duroure@gmail.com>
2015-10-13 22:01:01 +0200

    Correct reload addon

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@
 #======================= END GPL LICENSE BLOCK ========================
 bl_info = {
 	"name": "Extra Groups",
-	"version": (0, 0, 1),
+	"version": (0, 1),
 	"author": "Julien Duroure",
 	"blender": (2, 75, 0),
 	"description": "Extra Groups Tools",
@@ -26,23 +26,49 @@ bl_info = {
 	"tracker_url": "https://github.com/julienduroure/extragroups",
 	"category": "Animation"
 }
-	
-if "bpy" in locals():
-	import importlib
+
+import importlib
+import bpy
+
+if "ops_grouptype" in locals():	
 	importlib.reload(ops_grouptype)
+else:
+	from . import ops_grouptype
+if "ops_bonegroup" in locals():
 	importlib.reload(ops_bonegroup)
+else:
+	from . import ops_bonegroup
+if "ops_ops" in locals():
 	importlib.reload(ops_ops)
+else:
+	from . import ops_ops
+if "ops_template" in locals():
 	importlib.reload(ops_template)
+else:
+	from . import ops_template
+if "ops_text" in locals():
 	importlib.reload(ops_text)
+else:
+	from . import ops_text
+if "ui_list" in locals():
 	importlib.reload(ui_list)
+else:
+	from . import ui_list
+if "ui_panel" in locals():
 	importlib.reload(ui_panel)
+else:
+	from . import ui_panel
+if "template_default_ops" in locals():
 	importlib.reload(template_default_ops)
+else:
+	from . import template_default_ops
+if "text_ops_squel" in locals():
 	importlib.reload(text_ops_squel)
 else:
-	from . import ( ops_grouptype, ops_bonegroup, ops_ops, ops_template, ops_text, ui_list, ui_panel, template_default_ops )
+	from . import text_ops_squel
 
 
-import bpy
+
 import bpy.utils.previews
 import os
 
