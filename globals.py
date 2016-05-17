fe0b37833fc2a8d6aadac8c2ff96099c0a8a327b
Julien Duroure <julien.duroure@gmail.com>
2016-05-17 08:39:37 +0200

    GPL header

--- globals.py
+++ globals.py
@@ -1,3 +1,25 @@
+##########################################################################################
+#	GPL LICENSE:
+#-------------------------
+# This file is part of ExtraGroups.
+#
+#    ExtraGroups is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    ExtraGroups is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with ExtraGroups.  If not, see <http://www.gnu.org/licenses/>.
+##########################################################################################
+#
+#	Copyright 2016 Julien Duroure (contact@julienduroure.com)
+#
+##########################################################################################
 import bpy
 from bpy.app.handlers import persistent
 
--- template_default_ops.py
+++ template_default_ops.py
@@ -1,20 +1,25 @@
-#====================== BEGIN GPL LICENSE BLOCK ======================
+##########################################################################################
+#	GPL LICENSE:
+#-------------------------
+# This file is part of ExtraGroups.
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
+#    ExtraGroups is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+#    ExtraGroups is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#    You should have received a copy of the GNU General Public License
+#    along with ExtraGroups.  If not, see <http://www.gnu.org/licenses/>.
+##########################################################################################
 #
-#======================= END GPL LICENSE BLOCK ========================
+#	Copyright 2016 Julien Duroure (contact@julienduroure.com)
+#
+##########################################################################################
 import bpy
 
 from bpy.props import (
