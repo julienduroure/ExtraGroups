7559b2394c7d3d169b028182d92ba838024d9065
Julien Duroure <julien.duroure@gmail.com>
2017-05-12 15:11:25 +0200

    do not display popup if update needed

--- ui_popup.py
+++ ui_popup.py
@@ -63,7 +63,7 @@ class POSE_OT_jueg_popup(bpy.types.Operator):
     @classmethod
     def poll(self, context):
         armature = context.object
-        return addonpref().enable_popup == True and len(armature.jueg_grouptypelist) > 0 and len(armature.jueg_grouptypelist[armature.jueg_active_grouptype].group_ids) > 0
+        return check_addon_update_needed() == False and addonpref().enable_popup == True and len(armature.jueg_grouptypelist) > 0 and len(armature.jueg_grouptypelist[armature.jueg_active_grouptype].group_ids) > 0
 
 class POSE_OT_jueg_popup_toggle_multitype(bpy.types.Operator):
     bl_idname = "extragroups.popup_toggle_multitype"
